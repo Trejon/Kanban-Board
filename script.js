c42fe9fdb1cf04a2c165883ed0f64a2ec74a7453
Trejon <trejonstallsworth@gmail.com>
2020-07-26 15:26:11 -0600

    finished the core functionality of the project

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ let listArrays = [];
 
 // Drag Functionality
 let draggedItem;
+let dragging = false;
 let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
@@ -115,10 +116,14 @@ function updateDOM() {
 function updateItem(id, column) {
   const selectedArray = listArrays[column]; 
   const selectedColumnEl = listColumns[column].children; 
-  if (!selectedColumnEl[id].textContent) {
-    delete selectedArray[id]; 
+  if (!dragging) {
+    if (!selectedColumnEl[id].textContent) {
+      delete selectedArray[id]; 
+    } else {
+      selectedArray[id] = selectedColumnEl[id].textContent;
+    }
+    updateDOM();
   }
-  updateDOM();
 }
 
 // Add to Column List, Reset Text box 
@@ -169,6 +174,7 @@ function rebuildArrays() {
 // When Item Starts Dragging
 function drag(e) {
   draggedItem = e.target;
+  dragging = true;
 }
 
 // When Item Enters Column Area
@@ -192,6 +198,8 @@ function drop(e) {
   // Add Item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  // Dragging Complete
+  dragging = false;
   rebuildArrays();
 }
 
